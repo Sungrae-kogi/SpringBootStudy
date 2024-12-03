b2b2d589d7091506ced4bd0da7027e91f8769453
Josh_corgi <whvkek@naver.com>
2024-12-04 02:32:57 +0900

    [init] - project 생성, 더미 데이터 입력방법

---
+++ src/main/java/com/josh/springbootdeveloper/Member.java
@@ -0,0 +1,19 @@
+package com.josh.springbootdeveloper;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
