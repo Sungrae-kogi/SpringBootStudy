8b67194768d0c7f2246506495dde4a542a91e6d6
Josh_corgi <whvkek@naver.com>
2024-12-17 01:21:06 +0900

    [add] - JPA update 특징 : 트랜잭션과 변경감지

--- src/test/java/com/josh/springbootdeveloper/MemberRepositoryTest.java
+++ src/test/java/com/josh/springbootdeveloper/MemberRepositoryTest.java
@@ -38,4 +38,23 @@ class MemberRepositoryTest {
         //then
         assertThat(member.getId()).isEqualTo(3);
     }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void update(){
+        /*
+            정상동작하는 이유는 @DataJpaTest 어노테이션이 자동으로 트랜잭션 관리를 설정하기때문.
+            따라서 서비스 코드에서 업데이트 기능을 사용하려면, 반드시 서비스 메소드에 @Transactional을 붙여야 함.
+            수정 -> 조회 후 트랜잭션 범위 내에서 필드값 변경.
+         */
+
+        //given
+        Member member = memberRepository.findById(2L).get();
+
+        //when
+        member.changeName("BC");
+
+        //then
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
+    }
 }
