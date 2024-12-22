091e95594091e1eb617a5978042ca6004f712677
Josh_corgi <whvkek@naver.com>
2024-12-23 02:21:47 +0900

    [add] Controller 작성, 테스트

--- demo/src/main/java/com/example/demo/dto/AddArticleRequest.java
+++ demo/src/main/java/com/example/demo/dto/AddArticleRequest.java
@@ -14,7 +14,8 @@ public class AddArticleRequest {
 
     private String content;
 
-    //DTO는 계층 간 데이터 교환을 위한 객체, Article 엔티티로 변환하는 메소드 정도만 보유. 별도의 비즈니스 로직은 x
+    // DTO는 계층 간 데이터 교환을 위한 객체, Article 엔티티로 변환하는 메소드 정도만 보유. 별도의 비즈니스 로직은 x
+    // 계층 간 데이터 전달을 목적으로, 반환 타입이 Article 인것은 Article 데이터를 계층끼리 전달하겠다는 의도.
     public Article toEntity(){
         return Article.builder()
                 .title(title)
